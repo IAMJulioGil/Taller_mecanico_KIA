33456507280bef02e4d767f4a958950b1e87cfc6
IAMJulioGil <JulioGICJ@gmail.com>
2025-03-06 16:54:26 -0600

    Corregir nombres de archivos y agregar nuevos archivos

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.Taller_mecanico_KIA import iniciar_taller
+
+iniciar_taller()
--- src/Taller_mecanico_KIA.py
+++ src/Taller_mecanico_KIA.py
@@ -1,76 +1,77 @@
-BIENVENIDA="""
-Bienvenido este es un programa que simula el inventario de vehiculos del area del mantenimiento
-de una agencia de KIA motors
-"""
-MODELOS=""" Estos son los modelos que maneja la marca
-Sedanes: Rio, K3, K4, Optima, Stinger 
-SUVS: EV6, Sportage e-HYBRID, Sonet, Telluride, Seltos, Niro, Sorento, Carnival"""
-DESPEDIDA="""Saliendo...
-Gracias por usar este programa, este programa fue desarrollado por JulioGilzChang"""
-ADVERTENCIA= """No se Agregó el vehiculo 
-ya que no se encuentra en la lista de los modelos que te enseñamos en un principio,
-o puede que hayas escrito el nombre mal, favor de escribirlo tal cual se muestra en la lista.
-Intentelo de nuevo.
-"""
-vacio="""La lista de los vehiculos del área del mantenimiento está vacío 
-agregue un modelo de vehiculo de la lista que se le mostró.
-"""
+def iniciar_taller():
+    BIENVENIDA="""
+    Bienvenido este es un programa que simula el inventario de vehiculos del area del mantenimiento
+    de una agencia de KIA motors
+    """
+    MODELOS=""" Estos son los modelos que maneja la marca
+    Sedanes: Rio, K3, K4, Optima, Stinger 
+    SUVS: EV6, Sportage e-HYBRID, Sonet, Telluride, Seltos, Niro, Sorento, Carnival"""
+    DESPEDIDA="""Saliendo...
+    Gracias por usar este programa, este programa fue desarrollado por JulioGilzChang"""
+    ADVERTENCIA= """No se Agregó el vehiculo 
+    ya que no se encuentra en la lista de los modelos que te enseñamos en un principio,
+    o puede que hayas escrito el nombre mal, favor de escribirlo tal cual se muestra en la lista.
+    Intentelo de nuevo.
+    """
+    vacio="""La lista de los vehiculos del área del mantenimiento está vacío 
+    agregue un modelo de vehiculo de la lista que se le mostró.
+    """
 
-inventario=[]
+    inventario=[]
 
-concesionario= ["Rio", "K3","K4", "Optima", "Stinger",
-"EV6", "Sportage e-HYBRID", "Sonet" , "Telluride", "Seltos", "Niro" ,"Sorento", "Carnival"]
+    concesionario= ["Rio", "K3","K4", "Optima", "Stinger",
+    "EV6", "Sportage e-HYBRID", "Sonet" , "Telluride", "Seltos", "Niro" ,"Sorento", "Carnival"]
 
-OPCIONES="""
-Opciones:
-1)Agregar un vehiculo
-2)Quitar un vehiculo
-3)Mostrar inventario actualizado
-4)salir 
+    OPCIONES="""
+    Opciones:
+    1)Agregar un vehiculo
+    2)Quitar un vehiculo
+    3)Mostrar inventario actualizado
+    4)salir 
 
-"""
-print(BIENVENIDA)
-print(MODELOS)
+    """
+    print(BIENVENIDA)
+    print(MODELOS)
 
-while True: 
-    print(OPCIONES)
-    opcion=input(" Elige una opcion del (1 al 4): ")
+    while True: 
+        print(OPCIONES)
+        opcion=input(" Elige una opcion del (1 al 4): ")
 
-    if opcion == "1":
-        while True:
-            print("\ningresa el modelo que quieres agregar: " )
-            modelo=input("Modelo ").strip()
+        if opcion == "1":
+            while True:
+                print("\ningresa el modelo que quieres agregar: " )
+                modelo=input("Modelo ").strip()
 
-            if modelo in concesionario:
-                inventario.append(modelo)
-                print(f" Modelo {modelo} agregado al inventario. ")
-                break
-            else:
-                print(ADVERTENCIA)
+                if modelo in concesionario:
+                    inventario.append(modelo)
+                    print(f" Modelo {modelo} agregado al inventario. ")
+                    break
+                else:
+                    print(ADVERTENCIA)
 
-    elif opcion == "2":
-        
-            print(" \nIngresa el modelo del vehiculo que quieres quitar de la lista: ")
-            modelo = input(" modelo: ").strip()
+        elif opcion == "2":
+            
+                print(" \nIngresa el modelo del vehiculo que quieres quitar de la lista: ")
+                modelo = input(" modelo: ").strip()
+                if not inventario:
+                    print(vacio)
+                elif modelo in inventario:
+                    inventario.remove(modelo)
+                    print(f" Vehiculo {modelo} eliminado del inventario. ")
+                else:
+                    print(f"El vehiculo {modelo} no se encuentra en la lista ")
+
+        elif opcion == "3":
             if not inventario:
                 print(vacio)
-            elif modelo in inventario:
-                inventario.remove(modelo)
-                print(f" Vehiculo {modelo} eliminado del inventario. ")
             else:
-                print(f"El vehiculo {modelo} no se encuentra en la lista ")
+                print(" \n Inventario actualizado: ")
+            print(inventario)
 
-    elif opcion == "3":
-        if not inventario:
-            print(vacio)
+        elif opcion == "4":
+            print(DESPEDIDA)
+            break
+
         else:
-            print(" \n Inventario actualizado: ")
-        print(inventario)
-
-    elif opcion == "4":
-        print(DESPEDIDA)
-        break
-
-    else:
-        print("Opcion no valida. Por favor ingrese oun numero del 1 al 4")
+            print("Opcion no valida. Por favor ingrese oun numero del 1 al 4")
 
